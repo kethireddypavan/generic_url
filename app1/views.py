3159f885fa05c78c687f90a83151f823faf5b559
pavan10 <pavankumarreddy646@gmail.com>
2023-11-27 17:34:29 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pavan(request):
+    return render(request,'pavan.html')
+
+def rohit(request):
+    return render(request,'rohit.html')
